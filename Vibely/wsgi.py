5d19aab2b878c2a49336a127f9a3bf31dbae8f24
Kunal-K-Kamat <kunalkamat24@gmail.com>
2025-05-30 17:47:19 +0530

    Updated wsgi.py

--- Vibely/wsgi.py
+++ Vibely/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Book.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vibely.settings')
 
 application = get_wsgi_application()
